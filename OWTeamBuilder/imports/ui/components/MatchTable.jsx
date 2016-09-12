2f1272d3b7701239be5ec833a8d2d46b2947280a
Cameron <ctlanier@gmail.com>
2016-09-11 20:50:26 -0400

    Added Trial Data to the MatchTable

--- OWTeamBuilder/imports/ui/components/MatchTable.jsx
+++ OWTeamBuilder/imports/ui/components/MatchTable.jsx
@@ -10,6 +10,51 @@ class MatchTable extends Component {
  		};
  	}
 
+  getMatchInfo() {
+    let mathData = [
+      {
+        _id: 1,
+        mapName: 'Hanamura',
+        type: 'Assault',
+        result: w,
+        date: '9/11/2016',
+        skillDiff: '-300',
+        memberOne: 'Camlani#1682',
+        memberTwo: 'Enygmatic#11873',
+        memberThree: 'jaydotjaypeg#1304',
+        memberFour: 'Drew#1487',
+        memberFive: 'tcwontonsoup#1868',
+        memberSix: 'TaylorMade#12821',
+        },
+      {
+        _id: 2,
+        mapName: 'Temple of Anubis',
+        type: 'Assault',
+        result: l,
+        date: '9/11/2016',
+        skillDiff: '-300',
+        memberOne: 'Camlani#1682',
+        memberTwo: 'Enygmatic#11873',
+        memberThree: 'jaydotjaypeg#1304',
+        memberFour: 'Drew#1487',
+        memberFive: 'tcwontonsoup#1868',
+        memberSix: 'TaylorMade#12821',
+      }
+    ]
+    return matchData;
+
+  }
+
+  renderMatchRows() {
+    //this is where I need to have some sort of basic numbers
+
+
+
+    return (
+      <div/>
+    )
+  }
+
  	render() {
  		return (
  			<div className ="MatchTable">
@@ -30,7 +75,7 @@ class MatchTable extends Component {
               <th>Team Member 5</th>
               <th>Team Member 6</th>
             </tr>
-            
+
           </thead>
         </Table>
 
